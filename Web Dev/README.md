2ea7d7d09737dd6fab0df2c3a8760e8ef93fd819
infused Ideas <brent.larzalere@gmail.com>
2019-09-10 11:40:44 -0500

    Add files via upload

---
+++ Web Dev/README.md
@@ -0,0 +1,14 @@
+# Web Development
+
+This repo contains some websites I've developed using HTML5, CSS3, Javascript and Boostrap (3 & 4). All sites utilize responsive web design for presentation across mutliple device platforms.
+
+* restaurant - Proof of concept site for a restaurant.  Live link: http://phood.infusedideas.com/
+
+* newsletter - One-page site for sending newsletter style updates to clients.  Live link: http://newsletter.infusedideas.com/
+
+* architecture - Website of an architecture design firm. I was primarily looking to play around with bottom menu site navigation.  Live link: http://architecture.infusedideas.com/
+
+* profile - Single page marketing applicaiton for business or personal branding.  Live link: http://profile.infusedideas.com/
+
+
+
---
+++ Web Dev/architecture/js/main.js
@@ -0,0 +1,76 @@
+// my Javascripts =========================
+
+'use strict';
+
+// script to add swipe functionality to BS carousel ===================================
+		$(document).ready(function() {
+		   $("#myCarousel").swiperight(function() {
+			  $(this).carousel('prev');
+			});
+		   $("#myCarousel").swipeleft(function() {
+			  $(this).carousel('next');
+		   });
+		});
+
+// script to address modal backdrop bug for tall modals ===================================
+		$('body').on('shown.bs.modal', '.modal', function(){
+			var windowHeight = parseInt($(window).height());
+			var height = parseInt($('.modal-content').height());
+			if(windowHeight > height) height = windowHeight;
+		$('.modal-backdrop').height(height+600);
+		});
+
+// isotope JS ==================================================================================================
+		// initializes isotope and defines the container that its in
+		$(window).load(function(){
+				var $container = $('.portfolioContainer');
+				$container.isotope({
+					filter: '*',
+					animationOptions: {
+						duration: 750,
+						easing: 'linear',
+						queue: false
+					}
+				});
+
+				// used to dynamically recalcuate when the window is resized
+				$(window).resize(function () {
+					$container
+					  .isotope('reLayout');
+					return false;
+				});
+
+			 	// define filter categories and tie it to a click event
+				$('.portfolioFilter a').click(function(){
+					$('.portfolioFilter .current').removeClass('current');
+					$(this).addClass('current');
+
+					var selector = $(this).attr('data-filter');
+					$container.isotope({
+						filter: selector,
+						animationOptions: {
+							duration: 750,
+							easing: 'linear',
+							queue: false
+						}
+					 });
+					 return false;
+				});
+			});
+
+// mobile device detection for hidden section toggle  ======================================================
+		if (Modernizr.touch) {
+			$('#separator').addClass('hidden');
+			$('.thumbnail #newsMask').addClass('hidden');
+		} else {
+			$('#separator').removeClass('hidden');
+			$('.thumbnail #newsMask').removeClass('hidden');
+		}
+
+// parallax initiation ============================================================================================================
+
+		$(document).ready(function() {
+			$('.separator1').parallax("50%", 0.2);
+		});
+
+// ===================================================================================================================
